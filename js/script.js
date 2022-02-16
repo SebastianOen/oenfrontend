ac924c2ecea116343df4b1392220ba58050eb566
SebastianOen <83211414+SebastianOen@users.noreply.github.com>
2022-02-16 15:17:22 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,31 +3,6 @@ const slideRight = document.querySelectorAll(".slide-in");
 const slideUp = document.querySelectorAll(".slide-up");
 const unobserveElement = document.querySelector(".unobserve");
 
-/*const appearOptions = {};
-
-const appearOnScroll = new IntersectionObserver(
-  function (entries, appearOnScroll) {
-    entries.forEach((entry) => {
-      if (!entry.isIntersecting) {
-        return;
-      } else {
-        entry.target.classList.add("appear");
-        appearOnScroll.unobserve(entry.target);
-      }
-    });
-  },
-
-  appearOptions
-);
-
-slideRight.forEach((slider) => {
-  appearOnScroll.observe(slider);
-});
-
-slideUp.forEach((slideUps) => {
-  appearOnScroll.observe(slideUps);
-});*/
-
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
