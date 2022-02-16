cb602cb77265139e7ef5500402fb31b36b50f342
SebastianOen <83211414+SebastianOen@users.noreply.github.com>
2022-02-16 15:16:59 +0100

    fixed observer

--- js/script.js
+++ js/script.js
@@ -1,23 +1,24 @@
 //Sliders//
 const slideRight = document.querySelectorAll(".slide-in");
 const slideUp = document.querySelectorAll(".slide-up");
+const unobserveElement = document.querySelector(".unobserve");
 
-const appearOptions = {};
+/*const appearOptions = {};
 
-const appearOnScroll = new IntersectionObserver(function (
-  entries,
-  appearOnScroll
-) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add("appear");
-      appearOnScroll.unobserve(entry.target);
-    }
-  });
-},
-appearOptions);
+const appearOnScroll = new IntersectionObserver(
+  function (entries, appearOnScroll) {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+
+  appearOptions
+);
 
 slideRight.forEach((slider) => {
   appearOnScroll.observe(slider);
@@ -25,4 +26,23 @@ slideRight.forEach((slider) => {
 
 slideUp.forEach((slideUps) => {
   appearOnScroll.observe(slideUps);
+});*/
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("appear", entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 0,
+  }
+);
+
+slideRight.forEach((slide) => {
+  observer.observe(slide);
+});
+
+slideUp.forEach((slide) => {
+  observer.observe(slide);
 });
